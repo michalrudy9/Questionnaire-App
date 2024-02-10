b09517600cad09213241489221702fef562ecd46
michalrudy9 <michallrudy9@gmail.com>
2024-02-10 13:50:56 +0100

    Added login form module

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Signal, inject, signal } from '@angular/core';
 import { NavigationBarComponent } from '../navigation-bar/navigation-bar.component';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ErrorMessage } from '../../models/error-message.model';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NavigationBarComponent, MatButtonModule],
+  imports: [NavigationBarComponent, MatButtonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  private formBuilder = inject(FormBuilder);
+  protected form!: FormGroup;
+  protected errorMessage!: Signal<ErrorMessage>;
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: this.formBuilder.control(null, Validators.email),
+      password: this.formBuilder.control(null, Validators.required),
+    });
+
+    this.errorMessage = signal<ErrorMessage>({ message: '' });
+  }
+
+  protected onSubmit() {
+    const formData = this.form.value;
+    if (!formData['email'] || !formData['password']) {
+      this.errorMessage = signal<ErrorMessage>({
+        message: 'Please enter e-mail and password!',
+      });
+    } else {
+      //...
+    }
+    this.form.reset();
+  }
+}
